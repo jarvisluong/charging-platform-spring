f3a51d8ee9052e8c1ad697a8db15d435656c0e14
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2022-08-02 22:47:47 +0300

    remove unused imports

--- src/main/java/com/jarvisluong/chargingmap/controllers/HealthCheckController.java
+++ src/main/java/com/jarvisluong/chargingmap/controllers/HealthCheckController.java
@@ -1,6 +1,5 @@
 package com.jarvisluong.chargingmap.controllers;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
