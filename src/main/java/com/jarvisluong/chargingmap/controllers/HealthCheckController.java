7088581c8258706e10391177c9ddb18650ec9048
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2022-08-02 22:43:56 +0300

    add health check endpoint

---
+++ src/main/java/com/jarvisluong/chargingmap/controllers/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.jarvisluong.chargingmap.controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public Map<String, String> check() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "ok");
+        return map;
+    }
+}
