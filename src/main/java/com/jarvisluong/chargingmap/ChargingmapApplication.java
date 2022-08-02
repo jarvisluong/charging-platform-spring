f6f2fe4cbcfd777458924a717367741fdf36baa6
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2022-08-02 22:24:27 +0300

    init spring app

---
+++ src/main/java/com/jarvisluong/chargingmap/ChargingmapApplication.java
@@ -0,0 +1,13 @@
+package com.jarvisluong.chargingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargingmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarvisluong/chargingmap/ChargingmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvisluong.chargingmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
